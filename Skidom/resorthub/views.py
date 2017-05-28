53abe8af01409ee16beeecff73ebf973b56ccf8a
Rachelle Mariano <rachelle.mariano@gmail.com>
2017-05-28 00:31:18 -0400

    Add resort table

--- Skidom/resorthub/views.py
+++ Skidom/resorthub/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+<<<<<<< HEAD
 
 # Create your views here.
+=======
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, skiing world")
+
+>>>>>>> 86137960a9e8920087d8b0b16883e96d00eea37c
