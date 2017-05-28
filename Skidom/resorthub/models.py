a8869aa9b72d13e8fe67b1495899943396f86588
Daniel Richter <drichter9293@gmail.com>
2017-05-28 18:20:56 -0400

    Add snowfall columns to resort table

--- Skidom/resorthub/models.py
+++ Skidom/resorthub/models.py
@@ -10,7 +10,18 @@ class Resort(models.Model):
     rental_price = models.DecimalField(max_digits = 5, decimal_places = 2)
     season_begins = models.DateField(auto_now = True)
     maxx_pass = models.BooleanField(default = True) 
+    one_day_snowfall = models.IntegerField(default = 0)
+    two_day_snowfall = models.IntegerField(default = 0)
+    three_day_snowfall = models.IntegerField(default = 0)
+    four_day_snowfall = models.IntegerField(default = 0)
+    five_day_snowfall = models.IntegerField(default = 0)
+    six_day_snowfall = models.IntegerField(default = 0)
+    seven_day_snowfall = models.IntegerField(default = 0)
+    eight_day_snowfall = models.IntegerField(default = 0)
+    nine_day_snowfall = models.IntegerField(default = 0)
+    ten_day_snowfall = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.resort_name
+
 
