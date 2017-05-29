14fd777e0f8befba4336a12a59aebce071d075e1
Rachelle Mariano <rachelle.mariano@gmail.com>
2017-05-29 12:34:18 -0400

    Settings and db update

--- Skidom/Skidom/settings.py
+++ Skidom/Skidom/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'resorthub.apps.ResorthubConfig'
+    'resorthub.apps.ResorthubConfig',
+    'multiselectfield',
+    'address'
 ]
 
 MIDDLEWARE = [
