1c057d7972303ca96d0b3ea8ea0b2657850ce204
Rachelle Mariano <rachelle.mariano@gmail.com>
2017-05-28 22:06:41 -0400

    Made views modular and implemented error handling for invalid addresses

--- Skidom/resorthub/views.py
+++ Skidom/resorthub/views.py
@@ -22,29 +22,48 @@ def index(request):
         form = UserAddressForm(request.POST)
 
         if form.is_valid():
-            address = form.cleaned_data['user_address']
-
-            date = form.cleaned_data['search_date']
-            maxx_only = form.cleaned_data['maxx_pass_only']
-            
-            if maxx_only:  
-                resortlist = Resort.objects.filter(maxx_pass=True).order_by('resort_name')
-
-            resort_addresses = [x.resort_address for x in resortlist] 
-
-            json_map_dists = gmaps.distance_matrix(origins = address, destinations = resort_addresses, 
-                        mode = "driving", units = "imperial")
- 
-            clean_map_dists = []
-            
-            for i in range(0, len(resort_addresses)):
-                clean_map_dists.append(json_map_dists['rows'][0]['elements'][i]['distance']['text']) 
-                  
-            return render(request, 'resorthub/index.html', {'form': form, 'supported_resorts': resortlist, 'input_flag': 1, 'address': address, 'date': date, 'maxx_only': maxx_only, 'distances' : clean_map_dists})
+            form_dict = process_form(request, form, resortlist)
+            return render(request, 'resorthub/index.html', form_dict)
 
         else:
             return render(request, 'resorthub/index.html', {'form': form, 'supported_resorts': resortlist})
 
     else:
         form = UserAddressForm() 
-        return render(request, 'resorthub/index.html', {'form': form, 'supported_resorts': resortlist}) 
+        return render(request, 'resorthub/index.html', {'form': form, 'supported_resorts': resortlist})
+
+
+def process_form(request, form, resortlist):
+    address = form.cleaned_data['user_address']
+    date = form.cleaned_data['search_date']
+    maxx_only = form.cleaned_data['maxx_pass_only']
+            
+    if maxx_only:  
+        resortlist = Resort.objects.filter(maxx_pass=True).order_by('resort_name')
+
+    resort_addresses = [x.resort_address for x in resortlist] 
+
+    clean_dists, clean_times = use_googlemaps(address, resort_addresses)
+
+    if not clean_dists:
+        return({'invalid_address': 1, 'form': form, 'supported_resorts': resortlist, 'maxx_only': maxx_only})
+
+    return({'form': form, 'address': address, 'date': date, 'supported_resorts': resortlist, 'maxx_only': maxx_only,  'distances': clean_dists, 'times': clean_times})
+
+
+def use_googlemaps(address, resort_addresses):
+    json_map_dists = gmaps.distance_matrix(origins = address, destinations = resort_addresses, mode = "driving", units = "imperial")
+
+ 
+    clean_map_dists = []
+    clean_map_times = [] 
+
+    try: 
+        for i in range(0, len(resort_addresses)):
+            clean_map_dists.append(json_map_dists['rows'][0]['elements'][i]['distance']['text']) 
+            clean_map_times.append(json_map_dists['rows'][0]['elements'][i]['duration']['text']) 
+    
+    except KeyError:
+         return([], [])
+
+    return(clean_map_dists, clean_map_times) 
