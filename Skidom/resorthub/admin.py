0b6293b7c7a62a48d4d4b47d7b99a1ac92d2681d
Rachelle Mariano <rachelle.mariano@gmail.com>
2017-06-07 09:55:28 -0400

    Add admin control over accounts

--- Skidom/resorthub/admin.py
+++ Skidom/resorthub/admin.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Resort
+from .models import UserProfile
 
 admin.site.register(Resort)
-
+admin.site.register(UserProfile)    
