7c599538d96af94b883851606f70f7d2e55094c2
Rachelle Mariano <rachelle.mariano@gmail.com>
2017-09-17 10:47:51 -0400

    Address review concerns for resort_list

--- resorts/tests/test_resort_list.py
+++ resorts/tests/test_resort_list.py
@@ -1,23 +1,23 @@
 from django.test import TestCase
 
-from ..models import Resort, Conditions
+from ..models import Resort
 from ..views import resort_list
 
-USER_ADDRESS = '99 Brainerd Rd., Allston MA 02134'
+TEST_USER_ADDRESS = '99 Brainerd Rd., Allston MA 02134'
 
 class ResortListTestCase(TestCase):
     
     def setUp(self):
         Resort.objects.create(name="Hyrule", address="10 Rockefeller Plaza, New York, NY 10020")
-        Resort.objects.create(name="Wachusset", address="499 Mountain Rd, Princeton, MA 01541")
+        Resort.objects.create(name="Wachusett", address="499 Mountain Rd, Princeton, MA 01541")
         Resort.objects.create(name="Okemo", address="77 Okemo Ridge Rd, Ludlow, VT 05149")
         Resort.objects.create(name="Elvendale", address="1245 Worcester St, Natick Mall, Suite 2100, Natick, MA 01760")
 
-    def test_on_distance(self):
-        self.assertEquals(resort_list.get_resort_list(USER_ADDRESS, Resort.objects.all(),  order_on='distance'), [(u'Elvendale', {u'distance': 17.4, u'text_time': u'26 mins', u'time_in_seconds': 1576, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}), (u'Wachusset', {u'distance': 56.5, u'text_time': u'1 hour 8 mins', u'time_in_seconds': 4095, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}), (u'Okemo', {u'distance': 146.0, u'text_time': u'2 hours 54 mins', u'time_in_seconds': 10456, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}), (u'Hyrule', {u'distance': 209.0, u'text_time': u'3 hours 36 mins', u'time_in_seconds': 12936, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'})])
+    def test_order_on_distance(self):
+        self.assertEquals(resort_list.get_resort_list(Resort.objects.all(), user_address=TEST_USER_ADDRESS, order_on='distance'), [{u'distance': 17.4, u'text_time': u'26 mins', u'name': u'Elvendale', u'time_in_seconds': 1576, u'id': 4, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}, {u'distance': 56.5, u'text_time': u'1 hour 8 mins', u'name': u'Wachusett', u'time_in_seconds': 4095, u'id': 2, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}, {u'distance': 146.0, u'text_time': u'2 hours 54 mins', u'name': u'Okemo', u'time_in_seconds': 10456, u'id': 3, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}, {u'distance': 209.0, u'text_time': u'3 hours 36 mins', u'name': u'Hyrule', u'time_in_seconds': 12936, u'id': 1, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}])
 
-    def test_on_snow(self):
-        self.assertEquals(resort_list.get_resort_list(USER_ADDRESS, Resort.objects.filter(pk__lte=2), number_to_display = 15), [(u'Hyrule', {u'distance': 209.0, u'text_time': u'3 hours 36 mins', u'time_in_seconds': 12936, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}), (u'Wachusset', {u'distance': 56.5, u'text_time': u'1 hour 8 mins', u'time_in_seconds': 4095, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'})]) 
+    def test_order_on_snow(self):
+        self.assertEquals(resort_list.get_resort_list(Resort.objects.all(), number_to_display = 2), [{u'base_temp': u'N/A', u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'name': u'Hyrule', u'id': 5}, {u'base_temp': u'N/A', u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'name': u'Wachusett', u'id': 6}])
 
-    def test_on_time(self):
-        self.assertEquals(resort_list.get_resort_list(USER_ADDRESS, Resort.objects.all(), number_to_display= 10, order_on='time_in_seconds'), [(u'Elvendale', {u'distance': 17.4, u'text_time': u'26 mins', u'time_in_seconds': 1576, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}), (u'Wachusset', {u'distance': 56.5, u'text_time': u'1 hour 8 mins', u'time_in_seconds': 4095, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}), (u'Okemo', {u'distance': 146.0, u'text_time': u'2 hours 54 mins', u'time_in_seconds': 10456, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}), (u'Hyrule', {u'distance': 209.0, u'text_time': u'3 hours 36 mins', u'time_in_seconds': 12936, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'})]) 
+    def test_order_on_time(self):
+        self.assertEquals(resort_list.get_resort_list(Resort.objects.all(), user_address=TEST_USER_ADDRESS, number_to_display= 10, order_on='time_in_seconds'), [{u'distance': 17.4, u'text_time': u'26 mins', u'name': u'Elvendale', u'time_in_seconds': 1576, u'id': 12, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}, {u'distance': 56.5, u'text_time': u'1 hour 8 mins', u'name': u'Wachusett', u'time_in_seconds': 4095, u'id': 10, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}, {u'distance': 146.0, u'text_time': u'2 hours 54 mins', u'name': u'Okemo', u'time_in_seconds': 10456, u'id': 11, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}, {u'distance': 209.0, u'text_time': u'3 hours 36 mins', u'name': u'Hyrule', u'time_in_seconds': 12936, u'id': 9, u'snow_in_past_24h': u'N/A', u'num_trails_open': u'N/A', u'base_temp': u'N/A'}])
--- resorts/views/resort_list.py
+++ resorts/views/resort_list.py
@@ -1,44 +1,68 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-#General imports
+# General imports
 from collections import defaultdict
 from django.core.exceptions import ObjectDoesNotExist
 
-#Import Resort and ConditionsReport models
+# Import Resort and ConditionsReport models
 from ..models import Resort, Conditions
 
-#Imports for using Google Maps
+# Imports for using Google Maps
 import googlemaps
 import json
 
-#Google Maps requires a client key to work
+# Google Maps requires a client key to work
 GMAPS = googlemaps.Client(key='AIzaSyBRrCgnGFkdRY-Z1hX6xaxoUFBczNI2664')
 
+# Constants
+NUMBER_TO_DISPLAY = 5
+DEFAULT_ORDER_ON = 'snow_in_past_24h'
 
-def get_resort_list(user_address, selected_resorts, number_to_display=5, order_on='snow_in_past_24h'):
-#Given a user address and selected resorts, return an ordered list of resorts for display
-#We first use the Google Maps API to get distances and driving times for the journey between the user and the resorts
-#We then use our scraped objects to get the 24 hr snow fall, temperatures at the mountain base, and number of trails open
-#Finally, we return the sorted information as a list of tuples
+def get_resort_list(selected_resorts, user_address = "", number_to_display = NUMBER_TO_DISPLAY, order_on = DEFAULT_ORDER_ON):
+    """ Given a list of selected resorts, returns an ordered list of resorts and their conditions for display.
+        If a user_address is supplied, also returns driving time and distance information.
+    
+    Args:
+        selected_resorts (list): Queryset of Resorts
+        user_address (str, optional): Indicates the user address
+        number_to_display (int, optional): How many resorts we want to display
+        order_on (str, optional): How we want to order the resort list
 
-    resorts_info = defaultdict(dict)
-    use_googlemaps(user_address, selected_resorts, resorts_info)
-    get_conditions(selected_resorts, resorts_info) 
-    return(order_resorts(resorts_info, number_to_display, order_on))
+    Returns:
+        list: List of Resort dictionaries with name, id, condition information, and optional driving information
+
+    """
+    resorts_list = [x for x in selected_resorts.values('name', 'id')]
+
+    if user_address != "":
+        use_googlemaps(user_address, selected_resorts, resorts_list)
+
+    get_conditions(resorts_list) 
+    return(order_resorts(resorts_list, number_to_display, order_on))
 
 
-def use_googlemaps(address, selected_resorts, resorts_info):
-#We use the Google Maps API to get driving distances and times for the user
-#Google Maps returns a JSON response that we must parse
-#For how to response object looks, see: https://developers.google.com/maps/documentation/distance-matrix/intro
-#Grab the distance, chop off the trailing units using [:-3], and get rid of commas
-#This allows us to process it as float 
-#Keep the text-formatted times for output. It's human-readable (A days B hours C minutes)
-#Keep the time in seconds for easy sorting without conversion
+def use_googlemaps(address, selected_resorts, resorts_list):
+    """ We use the Google Maps API to get driving distances and times for the user.
+        Google Maps returns a JSON response that we must parse.
+        For how to response object looks, see: https://developers.google.com/maps/documentation/distance-matrix/intro.
+        We grab the distance, chop off the trailing units using [:-3], and get rid of commas.
+        This allows us to process the distance as a float. 
+        We keep the text-formatted times for display, as it's human-readable (A days B hours C minutes).
+        We keep the time in seconds for easy sorting.
 
-    for resort in selected_resorts:
-        json_map = GMAPS.distance_matrix(origins = address, destinations = resort.address.raw, mode = "driving", units = "imperial")
+    Args:
+        address (str): Indicates the user address
+        resorts_list (list): Queryset of Resorts
+
+    Returns:
+        None.
+        Resorts_list passed by reference. Relevant fields updated. 
+
+    """
+    
+    for resort in resorts_list:
+        json_map = GMAPS.distance_matrix(origins = address, destinations = selected_resorts.get(name=resort['name']).address.raw, mode = "driving", units = "imperial")
 
         try:
             dist = float(json_map['rows'][0]['elements'][0]['distance']['text'][:-3].replace(',', ''))
@@ -50,18 +74,27 @@ def use_googlemaps(address, selected_resorts, resorts_info):
             time_in_seconds = "N/A"
             text_time = "N/A"
 
-        resorts_info[resort.name]['distance'] = dist 
-        resorts_info[resort.name]['time_in_seconds'] = time_in_seconds
-        resorts_info[resort.name]['text_time'] = text_time                                  
+        resort['distance'] = dist 
+        resort['time_in_seconds'] = time_in_seconds
+        resort['text_time'] = text_time                                  
     
-def get_conditions(resort_list, resorts_info):
-#Gets the associated Conditions report and adds this information to 
-#Resorts info. If this information is not available (no Conditions object
-#available), fill in with N/A
+def get_conditions(resorts_list):
+    """ Gets the associated Conditions report and adds this information to 
+        Resorts info. If this information is not available (no Conditions object
+        available), fill in with N/A.
 
-    for resort in resort_list:
+    Args:
+        resorts_list (list): Queryset of Resorts
+
+    Returns:
+        None.
+        Resorts_list passed by reference. Relevant fields updated.
+    
+    """
+
+    for resort in resorts_list:
         try:
-            t = Conditions.objects.get(resort=resort)
+            t = Conditions.objects.get(resort=resort['id'])
             base_temp = str(int(t.base_temp))+"° F"
             num_trails_open = t.num_open
             snow_in_past_24h = t.new_snow            
@@ -71,28 +104,32 @@ def get_conditions(resort_list, resorts_info):
             num_trails_open = "N/A"
             snow_in_past_24h = "N/A"
 
-        resorts_info[resort.name]['base_temp'] = base_temp
-        resorts_info[resort.name]['num_trails_open'] = num_trails_open
-        resorts_info[resort.name]['snow_in_past_24h'] = snow_in_past_24h
+        resort['base_temp'] = base_temp
+        resort['num_trails_open'] = num_trails_open
+        resort['snow_in_past_24h'] = snow_in_past_24h
 
-def order_resorts(resorts_info, number_to_display, order_on):
-#We want the highest base temperatures, amount of snow, and number of trails open
-#We want the smallest distances and times
-#The decreasing flag accounts for this
-#Returns tuples of sorted resorts with (resort_name, resort_info for that resort)
+def order_resorts(resorts_list, number_to_display, order_on):
+    """ We want the highest base temperatures, amount of snow, and number of trails open.
+        We want the lowest distances and times. The decreasing flag accounts for this.
+        Sorts list based on order_on and returns a list with min(number_to_display, len(resorts_list)) elements
 
-    list_of_ordered_resorts = []
-    decreasing_flag = 1
+    Args:
+        resorts_list (list): Queryset of Resorts
+        number_to_display (int, optional): How many resorts we want to display
+        order_on (str, optional): How we want to order the resort list
 
-    if order_on == "alphabet":
-        keys = sorted(resorts_info)        
+    Returns:
+        resorts_list (list): List of Resort dictionaries sorted using order_on 
+                             with min(number_to_display, len(resorts_list)) elements 
 
-    elif order_on in ['time_in_seconds', 'distance']:
+
+    """    
+
+    decreasing_flag = 1   
+
+    if order_on in ['name', 'time_in_seconds', 'distance']:
             decreasing_flag = 0
 
-    keys = sorted(resorts_info, key = lambda x: resorts_info[x][order_on], reverse = decreasing_flag)
-
-    for i in range(min(number_to_display, len(keys))):
-        list_of_ordered_resorts.append((keys[i], resorts_info[keys[i]]))
-    
-    return(list_of_ordered_resorts) 
+    resorts_list.sort(key = lambda k: k[order_on], reverse = decreasing_flag)
+ 
+    return(resorts_list[:min(number_to_display, len(resorts_list))]) 
