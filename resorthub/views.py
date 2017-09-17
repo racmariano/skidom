db834c7c31e5629e7df8979e096b94cd0f5e424f
Rachelle Mariano <rachelle.mariano@gmail.com>
2017-09-17 13:12:41 -0400

    Add basic support for use of get_resort_list to resorthub views.py

--- resorthub/views.py
+++ resorthub/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-#General imports
+# General imports
 import re
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
@@ -10,24 +10,88 @@ from django.views import generic, View
 from django.contrib import messages  
 from django.core.exceptions import ObjectDoesNotExist
 
-#Libraries for user support
+# Libraries for user support
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
-#Libraries for distance/time estimates
-import googlemaps
-import json
-
-#Import Objects
+# Import Objects
 from .models import OldResort, TrailPage
 from .forms import UserAddressForm, CompareOrFavoriteForm
 
-#Global variables
-gmaps = googlemaps.Client(key='AIzaSyBRrCgnGFkdRY-Z1hX6xaxoUFBczNI2664')
+
+def index(request):
+    """ Home page for Skidom.
+
+    On a GET request: Displays form for getting user trip details. 
+    If user logged in and has favorite resorts, displays information
+    about the five of them with the most 24h snow.
+    Else, displays 5 resorts with the most 24h snow.
+
+    On a POST request: Processes user trip detail form.
+    Renders matching resorts and information to comparison template.
+
+    Args:
+        request: Page request.
+
+    Returns:
+        render: Renders valid resorts and info to comparison template
+                if successful. Else, reloads the page.
+    
+    """
+    resorts_list = OldResort.objects.all() 
+
+    if request.method == 'POST':
+        form = UserAddressForm(request.POST, pass_type = request.POST['pass_type'], starting_from = request.POST['user_address'])
+
+        if form.is_valid():
+            resorts_list = process_form(request, form)
+            return render(request, 'resorthub/compare_options.html', resorts_list)
+
+    else:
+        header_message = "Where we\'d ski this weekend:"
+
+        if request.user.is_authenticated():
+            address = request.user.address
+            pass_type = request.user.pass_type            
+            if len(request.user.favorite_resorts.all()) > 0:
+                header_message = "What\'s up with your favorite resorts:"
+                resorts_list = request.user.favorite_resorts.all()
+
+        else:
+            address = 'Let\'s go!'
+            pass_type = "NON"
+
+        resorts_list = get_resort_list(resorts_list, filter_on="new_snow")
+        form = UserAddressForm(pass_type = pass_type, starting_from=address)
+ 
+        return render(request, 'resorthub/index.html', {'form': form, 'header_message': header_message, 'supported_resorts': resorts_list})
 
 
-#COMPARE AND RESORT LISTING METHOOOODSSS!!!
+def process_form(request, form):
+    """ Processes trip information form to get resort list for display.
+
+    Args:
+        request (request): Page request
+        form (Form): Posted trip information form 
+
+    Returns:
+        list: List of Resort dictionaries sorted by user choice and filtered by the pass option the user selected. 
+              If no resorts with that pass are available, returns an empty list.
+
+    """
+    address = form.cleaned_data['user_address']
+    pass_info = form.cleaned_data['pass_type'][0]
+    sort_opt = form.cleaned_data['sort_opt']
+
+    filtered_resort_list = OldResort.objects.filter(available_passes__contains=pass_info)
+
+    if not filtered_resort_list:
+        return([])
+
+    return(get_resort_list(filtered_resort_list, user_address = address, number_to_display = len(filtered_resort_list), order_on = sort_opt)
+
+
 def resort_listing(request):
     if request.method == 'POST':
         selected_resort_ids = request.POST.getlist('choices[]')
@@ -64,140 +128,3 @@ def compare_listing(request, resort_list=OldResort.objects.all()):
         return render(request, 'resorthub/compare.html', {'resorts': resort_list})
 
 
-#RESORT HUB METHODS!!!!
-def get_top_five_resorts(resort_list, filter_on='num_open'):
-    resort_list, base_temps, num_open, new_snow = get_scraped_info(resort_list)
-    resort_info = zip(resort_list, base_temps, num_open, new_snow)    
-
-    if filter_on == 'num_open':
-        resort_info.sort(key = lambda t: t[1], reverse=True)
-    else:
-        resort_info.sort(key = lambda t: t[2], reverse=True)
-
-    if len(resort_info) >= 5:
-        end = 5
-    else:
-        end = len(resort_list)
-
-    return(zip(*resort_info[0:end]))
-
-def index(request):
-    resort_list = OldResort.objects.all() 
-    if request.method == 'POST':
-        form = UserAddressForm(request.POST, pass_type = request.POST['pass_type'], starting_from = request.POST['user_address'])
-
-        if form.is_valid():
-            form_dict = process_form(request, form, resort_list)
-            return render(request, 'resorthub/compare_options.html', form_dict)
-
-        else:
-            return render(request, 'resorthub/index.html', {'form': form, 'supported_resorts': resort_list})
-
-
-    else:
-        header_message = "Where we\'d ski this weekend:"
-        resort_list = OldResort.objects.exclude(url__exact='')
-
-        if request.user.is_authenticated():
-            address = request.user.address
-            pass_type = request.user.pass_type            
-            if len(request.user.favorite_resorts.all()) > 0:
-                header_message = "What\'s up with your favorite resorts:"
-                resort_list = request.user.favorite_resorts.all()
-
-        else:
-            address = 'Let\'s go!'
-            pass_type = "NON"
-
-        resort_list, base_temps, num_open, new_snow = get_top_five_resorts(resort_list, filter_on="new_snow")
-
-
-        form = UserAddressForm(pass_type = pass_type, starting_from=address) 
-        return render(request, 'resorthub/index.html', {'form': form, 'header_message': header_message, 'supported_resorts': resort_list, 'base_temps': base_temps, 'trails_open': num_open, 'new_snow': new_snow})
-
-
-def process_form(request, form, resort_list):
-    address = form.cleaned_data['user_address']
-    date = form.cleaned_data['search_date']
-    pass_info = form.cleaned_data['pass_type'][0]
-    sort_opt = form.cleaned_data['sort_opt']
-
-    #If we haven't made a crawler page, the url will be blank            
-    filtered_resort_list = OldResort.objects.filter(available_passes__contains=pass_info).exclude(url__exact='').order_by('name')
-
-    if not filtered_resort_list:
-        return({'no_match': 1, 'form': form, 'supported_resorts': resort_list})
-
-    resort_addresses = [x.address.raw for x in filtered_resort_list] 
-    clean_dists, clean_times = use_googlemaps(address, resort_addresses)
-
-    if not clean_dists:
-        return({'invalid_address': 1, 'form': form, 'supported_resorts': resort_list})
-
-    ordered_resort_info = order_resorts(sort_opt, filtered_resort_list, clean_dists, clean_times)
-    ordered_resorts, ordered_dists, ordered_times, ordered_temps, ordered_open, ordered_snow = zip(*ordered_resort_info)
-
-    return({'form': form, 'address': address, 'date': date, 'supported_resorts': ordered_resorts, 'distances': ordered_dists, 'times': ordered_times, 'base_temps': ordered_temps, 'trails_open':ordered_open, 'new_snow':ordered_snow,})
-
-
-def order_resorts(sort_opt, filtered_resort_list, clean_dists, clean_times):
-    filtered_resort_list, base_temps, num_open, new_snow = get_scraped_info(filtered_resort_list)
-    resort_info = zip(filtered_resort_list, clean_dists, clean_times, base_temps, num_open, new_snow)
-
-    #We want resorts with most snow, trails, warmest, etc. 
-    #However, we want resorts with shortest times
-    descending_order = 1
-    
-    if sort_opt == "ABC":
-        return(resort_info)    
-
-    elif sort_opt == "TIM":
-        return(time_order(resort_info))
-
-    elif sort_opt == "DIS":
-        i = 1
-        descending_order = 0
-
-    elif sort_opt == "WEA":
-        i = 3
-
-    elif sort_opt == "TRA":
-        i = 4
-
-    elif sort_opt == "SNO":
-        i = 5
-
-    resort_info.sort(key = lambda t: t[i], reverse=descending_order)
-
-    return(resort_info) 
-
-def time_order(resort_info):
-#Takes in zipped list of [(resorts, distances, times)] and converts string times to sum of minutes.
-#Returns sorted list based on sums of minutes.
-
-    minute_times = []
-
-    for resort in resort_info:
-        time_in_minutes = 0
-        google_time = resort[2]
-
-        if 'day' in google_time:
-            parsed_time = re.split('day[s]? ', google_time)
-            time_in_minutes += 1440*int(parsed_time[0])
-            google_time = parsed_time[1]
-
-        if 'hour' in google_time:
-            parsed_time = re.split('hour[s]?', google_time)
-            time_in_minutes += 60*int(parsed_time[0]) 
-            google_time = parsed_time[1]    
-
-        if 'min' in google_time:
-            parsed_time = re.split('min[s]?', google_time)
-            time_in_minutes += int(parsed_time[0]) 
-
-        minute_times.append(time_in_minutes)
-
-    resorts_with_times = zip(resort_info, minute_times)
-    resorts_with_times.sort(key = lambda t: t[1])
-    sorted_resorts = zip(*resorts_with_times)[:-1]
-    return(sorted_resorts[0])
